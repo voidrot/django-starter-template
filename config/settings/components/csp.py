8c51ab63ce66c7a66074065390a6e999b881581c
Buck Brady <22723438+voidrot@users.noreply.github.com>
2026-02-01 03:32:13 -0700

    feat: Refine Allauth templates, update Docker Compose environment variable loading, and introduce new testing infrastructure.

--- config/settings/components/csp.py
+++ config/settings/components/csp.py
@@ -2,16 +2,16 @@
 from csp.constants import NONE, SELF
 
 CONTENT_SECURITY_POLICY_REPORT_ONLY = {
-    "EXCLUDE_URL_PREFIXES": ["/excluded-path/"],
-    "DIRECTIVES": {
-        "default-src": [NONE],
-        "connect-src": [SELF],
-        "img-src": [SELF],
-        "form-action": [SELF],
-        "frame-ancestors": [SELF],
-        "script-src": [SELF],
-        "style-src": [SELF],
-        "upgrade-insecure-requests": True,
-        "report-uri": "/csp-report/",
+    'EXCLUDE_URL_PREFIXES': ['/excluded-path/'],
+    'DIRECTIVES': {
+        'default-src': [NONE],
+        'connect-src': [SELF],
+        'img-src': [SELF],
+        'form-action': [SELF],
+        'frame-ancestors': [SELF],
+        'script-src': [SELF],
+        'style-src': [SELF],
+        'upgrade-insecure-requests': True,
+        'report-uri': '/csp-report/',
     },
 }
